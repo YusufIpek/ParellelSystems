aa654576a1617ffea49861094e40883613d31a15
YusufIpek <yusuf.ipek2@gmail.com>
2017-10-13 22:53:56 +0200

    corrected bug in matrix multiplication

--- Exercise 1/Task3/mmul_v2.cpp
+++ Exercise 1/Task3/mmul_v2.cpp
@@ -23,15 +23,16 @@ Matrix id(unsigned n) {
 Matrix operator*(const Matrix& a, const Matrix& b) {
 	unsigned n = a.size();
 	Matrix c = id(n);
-	int tile = 32;
+	int tile = 64;
 	for(unsigned i=0; i<n; i+=tile) {
 		for(unsigned j=0; j<n; j+=tile) {
-			c[i][j] = 0;
 			for(unsigned k=0; k<n; k += tile) {
 				for(unsigned x = i; x < i+tile; x++){
 					for(unsigned y = j; y < j+tile; y++){
+						if(k == 0)
+							c[x][y] = 0;
 						for(unsigned z = k; z < k+tile; z++){
-							c[x][y] += a[x][z] * b[y][z];		
+							c[x][y] += a[x][z] * b[z][y];		
 						}
 					}					
 				}
@@ -41,6 +42,7 @@ Matrix operator*(const Matrix& a, const Matrix& b) {
 	}
 	return c;
 }
+
 
 
 int main(int argc, char** argv) {
@@ -59,16 +61,13 @@ int main(int argc, char** argv) {
 
 	// compute the product
 	auto c = a * b;
-
-
+	
 	// check that the result is correct
 	if(c == a){
 		double duration = (std::clock() - start) / (double) CLOCKS_PER_SEC;
 		std::cout << "Time: " << duration << std::endl;
 		return EXIT_SUCCESS;
 	}else{
-		double duration = (std::clock() - start) / (double) CLOCKS_PER_SEC;
-		std::cout << "Time: " << duration << std::endl;
 		return EXIT_FAILURE;
 	}
 }
