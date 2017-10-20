8aafa6f33920a36ae7ffa7dacbb9d96f636b8148
Wolfgang Leimer <csac9626@zid-t9pc1903.uibk.ac.at>
2017-10-20 17:53:51 +0200

    modified test.sh and added its output as logfile

--- exercise02/test.sh
+++ exercise02/test.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 export opt=3
+export repeats=5
+export seperator="--------------------------------------------------------------------------------------------------------"
 
 g++ mmul.cpp -Wall -std=c++11 -O$opt -o mmul_nes -DNESTED_VECTOR
 g++ mmul.cpp -Wall -std=c++11 -O$opt -o mmul_conmul -DCONTIGUOUS_WITH_MULTIPLICATION
 g++ mmul.cpp -Wall -std=c++11 -O$opt -o mmul_conin -DCONTIGUOUS_WITH_INDIRECTION
 
-for i in {0..1};
+for i in {0..2};
 do
-  for MSIZE in 256 512 1024 2048 4096;
+  for MSIZE in 256 512 1024 2048;
   do
-    perf stat ./mmul_nes $MSIZE &> log_nes_run-${i}_size-$MSIZE.txt
-    perf stat ./mmul_conmul $MSIZE &> log_conmul_run-${i}_size-$MSIZE.txt
-    perf stat ./mmul_conin $MSIZE &> log_conin_run-${i}_size-$MSIZE.txt
+    perf stat -d -r$repeats ./mmul_nes $MSIZE >>log_test.txt 2>&1
+    echo $seperator >>log_test.txt
+    perf stat -d -r$repeats ./mmul_conmul $MSIZE >>log_test.txt 2>&1
+    echo $seperator >>log_test.txt
+    perf stat -d -r$repeats ./mmul_conin $MSIZE >>log_test.txt 2>&1
+    echo $seperator >>log_test.txt
   done
-done  
+done
