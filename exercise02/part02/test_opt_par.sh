83e49be587bcdebe528ca853f61cbaab67943dce
Yusuf Ipek <csap7752@zid-gpl.uibk.ac.at>
2017-10-22 19:39:43 +0200

    bash script for benchmarking; log of bechnmark data

---
+++ exercise02/part02/test_opt_par.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export opt=3
+export repeats=5
+export input_file="mmul_opt_para.cpp"
+export filename="log_opt_para_test.txt"
+export seperator="--------------------------------------------------------------------------------------------------------"
+
+g++ $input_file -Wall -std=c++11 -O$opt -fopenmp -o mmul_nes -DNESTED_VECTOR
+g++ $input_file -Wall -std=c++11 -O$opt -fopenmp -o mmul_conmul -DCONTIGUOUS_WITH_MULTIPLICATION
+g++ $input_file -Wall -std=c++11 -O$opt -fopenmp -o mmul_conin -DCONTIGUOUS_WITH_INDIRECTION
+
+for i in {0..2};
+do
+  for MSIZE in 256 512 1024 2048;
+  do
+    perf stat -d -r$repeats ./mmul_nes $MSIZE >> $filename 2>&1
+    echo $seperator >>$filename
+    perf stat -d -r$repeats ./mmul_conmul $MSIZE >>$filename 2>&1
+    echo $seperator >>$filename
+    perf stat -d -r$repeats ./mmul_conin $MSIZE >>$filename 2>&1
+    echo $seperator >>$filename
+  done
+done
