fbe719204661e89771404fec699e9aa9edd13a95
Wolfgang Leimer <csac9626@zid-t9pc2212.uibk.ac.at>
2017-11-07 13:56:30 +0100

    testscript updated

--- exercise04/test_montecarlo.sh
+++ exercise04/test_montecarlo.sh
@@ -2,12 +2,13 @@
 
 g++ montecarlo_pi.cpp -o montecarlo_pi -Wall -O3 -std=c++11
 g++ montecarlo_pi_par.cpp -o montecarlo_pi_par -Wall -O3 -std=c++11 -fopenmp
-
+g++ montecarlo_pi_par_v2.cpp -o montecarlo_pi_par_v2 -Wall -O3 -std=c++11 -fopenmp
 
 for i in 10000 100000 1000000 10000000 100000000;
 do
   echo "N = "$i
   ./montecarlo_pi $i
   ./montecarlo_pi_par $i
+  ./montecarlo_pi_par_v2 $i
   echo ""
 done
