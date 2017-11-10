4e61ffbc74df59b7921364e85866dada7c02b3f1
YusufIpek <yusuf.ipek2@gmail.com>
2017-11-10 15:17:14 +0100

    Updated: correct result

--- exercise05/n_queens_par.cpp
+++ exercise05/n_queens_par.cpp
@@ -1,44 +1,51 @@
 #include <vector>
 #include <iostream>
+#include <omp.h>
 
 
 using Board = std::vector<int>;
 
 bool is_position_valid(const Board& board, int row, int column){
 	
-	
-	
-		for(int j = 0; j < row; j++){
-			
-				if(board[j]==column){
-					return false;
-				}
-				if((board[j]-(row - j)) == column){
-					return false;
-				}
-				if((board[j]+(row - j)) == column){
-					return false;
-				}	
-					
-		}
-	
+	for(int j = 0; j < row; j++){		
+			if(board[j]==column){
+				return false;
+			}
+			if((board[j]-(row - j)) == column){
+				return false;
+			}
+			if((board[j]+(row - j)) == column){
+				return false;
+			}	
+	}
 	return true;
 }
 
 
-void board_place_queens(Board board, int n, int queens, int row, int& solutions){
-	
+void board_place_queens_threads(Board board, int n, int queens, int row, int& solutions){
 	if(queens == 0){
-    #pragma omp atomic
-	  solutions++;
+		#pragma omp atomic
+		solutions++;
 		return;
 	}
 	
-  #pragma omp for schedule(dynamic)
 	for(int j = 0; j < n; j++){
 		if(is_position_valid(board, row, j)){
 			board[row] = j;
-			board_place_queens(board, n, queens-1, row+1, solutions);
+			board_place_queens_threads(board, n, queens-1, row+1, solutions);
+		}
+	}
+}
+
+void board_place_queens(int n, int queens, int row, int& solutions){
+	
+	#pragma omp parallel
+	{
+		auto board = Board(n);
+		#pragma omp for schedule(dynamic)
+		for(int j = 0; j < n; j++){
+			board[row] = j;
+			board_place_queens_threads(board, n, queens-1, row+1, solutions);	
 		}
 	}
 }
@@ -57,12 +64,9 @@ int main(int argc, char** argv){
 	int n = atoi(argv[1]);
 	int solutions = 0;
 	
-  #pragma omp parallel
-  {
-  	auto board = Board(n,0); //std::vector<int>(n, 0);
-  	
-  	board_place_queens(board, n, n, 0, solutions);
-  }
+  
+  	board_place_queens(n, n, 0, solutions);
+  
 	std::cout << "Solutions: " << solutions << std::endl;
 	
 	return 0;
