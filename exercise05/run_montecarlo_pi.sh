96b027d684bfa34c86a7646eced05f03e6c05615
YusufIpek <yusuf.ipek2@gmail.com>
2017-11-10 17:25:52 +0100

    scripts to run the programs

--- exercise05/run_montecarlo_pi.sh
+++ exercise05/run_montecarlo_pi.sh
@@ -2,15 +2,16 @@
 
 export opt=3
 
+
 for i in {100000,1000000,10000000,100000000}
 do
 	LC_NUMERIC=en_US printf "Problem size: %'.f\n" $i
 	printf "Seq "
-	./merge_sort $i
+	./montecarlo_pi $i
 	for j in {1,2,4,8}
 	do
 		printf "Par "$j" "
-		./merge_sort_par_$j $i
+		./montecarlo_pi_par_$j $i
 	done
 	printf "\n"
 done
