c29ab1bd16c63a8281f7662739e4ee38d8c9ce08
Wolfgang Leimer <csac9626@zid-t9pc2002.uibk.ac.at>
2017-11-10 15:57:28 +0100

    compile scripts

---
+++ exercise05/compile_mergesort.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if ! g++ merge_sort.cpp -o merge_sort -Wall -O3 -std=c++11;
+then
+  echo COMPILATION of merge_sort FAILED!
+  return 1
+fi
+
+if ! g++ merge_sort_par.cpp -o merge_sort_par -Wall -O3 -std=c++11 -fopenmp;
+then
+  echo COMPILATION of merge_sort_par FAILED!
+  return 1
+fi
+
+echo COMPILATION SUCCESSFUL
---
+++ exercise05/compile_montecarlo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if ! g++ montecarlo_pi.cpp -o montecarlo_pi -Wall -O3 -std=c++11;
+then
+  echo COMPILATION of montecarlo_pi FAILED!
+  return 1
+fi
+
+if ! g++ montecarlo_pi_par.cpp -o montecarlo_pi_par -Wall -O3 -std=c++11 -fopenmp;
+then
+  echo COMPILATION of montecarlo_pi_par FAILED!
+  return 1
+fi
+
+echo COMPILATION SUCCESSFUL
