4f4f447b9c99ea159a51b5c429d23461bcd8f9ae
YusufIpek <yusuf.ipek2@gmail.com>
2017-11-13 18:44:01 +0100

    Updated script for running montecarlo

--- exercise05/run_montecarlo_pi.sh
+++ exercise05/run_montecarlo_pi.sh
@@ -4,13 +4,13 @@
 
 for i in 100000 1000000 10000000 100000000 1000000000
 do
-	LC_NUMERIC=en_US printf "Problem size: %'.f\n" $i
+	printf "Problem size: %'.d\n" $i
 	printf "Seq "
 	./montecarlo_pi $i
 	for j in 1 2 4 8
 	do
 		export OMP_NUM_THREADS=$j
-		printf "Par "$j" "
+		printf "Number of Threads: "$j
 		./montecarlo_pi_par $i
 	done
 	printf "\n"
