4a8a4465025fe2238e68a7f185da4765f9dd7c82
wleimer <wleimer@ahm.co.at>
2017-11-30 18:21:01 +0100

    deletions

--- exercise08/jacobi.hpp
+++
@@ -1,8 +0,0 @@
-
-#if !defined(GRIDSIZE)
-#define GRIDSIZE 10
-#endif
-
-#if !defined(EPSILON)
-#define EPSILON 0.1
-#endif
--- exercise08/run_jacobi.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-{ time ./jacobi_2D_par 1 0.5 0 -0.5; } 2>&1
-
