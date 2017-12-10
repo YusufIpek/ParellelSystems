3f37554f2f0dd78af689f5223dc83c16802ddfdc
Yusuf Ipek <yusuf.ipek2@gmail.com>
2017-12-10 17:25:37 +0100

    small print adjustments

--- exercise09/run_multiple_nodes.sh
+++ exercise09/run_multiple_nodes.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 
-echo "Size: 512| Processes: " $1
+echo "Processes: " $1
 { time mpirun -np $1 ./jacobi_2D_512_mpi 1.0 0.5 0.0 -0.5; } 2>&1
-echo "Size: 768| Processes: " $1
+echo
+echo "Processes: " $1
 { time mpirun -np $1 ./jacobi_2D_768_mpi 1.0 0.5 0.0 -0.5; } 2>&1
-done
+
--- exercise09/run_single_node.sh
+++ exercise09/run_single_node.sh
@@ -2,8 +2,10 @@
 
 for i in 1 2 4 8
 do
-echo "Size: 512| Processes: " $i
+echo "Processes: " $i
 { time mpirun -np $i ./jacobi_2D_512_mpi 1.0 0.5 0.0 -0.5; } 2>&1
-echo "Size: 768| Processes: " $i
+echo
+echo "Processes: " $i
 { time mpirun -np $i ./jacobi_2D_768_mpi 1.0 0.5 0.0 -0.5; } 2>&1
+echo
 done
